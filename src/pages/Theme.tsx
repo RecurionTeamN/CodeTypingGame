f93d0db34245168a85d91b73d8013d6ff707cc1a
YIshihara11201 <yusukeishihara87@gmail.com>
2022-03-06 15:56:58 -0800

    #10 added Theme.tsx

---
+++ src/pages/Theme.tsx
@@ -0,0 +1,21 @@
+import { createTheme } from '@mui/material/styles';
+
+// A custom theme for this app
+const theme = createTheme({
+  palette: {
+    primary: {
+        main: '#77ffff',
+    },
+    secondary: {
+        main: '#8cb4c4',
+    },
+    error: {
+        main: '#bb0000',
+    },
+    background: {
+        default: '#fff',
+    },
+},
+});
+
+export default theme;
