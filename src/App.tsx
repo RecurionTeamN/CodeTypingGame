7c675bd009acd504d41eb962653e5e903472b8b5
hiroto0222 <aoyamahiroto0222@gmail.com>
2022-03-06 14:59:16 +0900

    #3 added eslint prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { Button, Card, CardActions, CardContent, Typography } from "@mui/material";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-          <Button variant="contained">button</Button>
-        </a>
-        <Card>
-          <CardContent>
-            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-              Word of the Day
-            </Typography>
-            <Typography variant="h5" component="div">
-              asdf
-            </Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-              adjective
-            </Typography>
-            <Typography variant="body2">
-              well meaning and kindly.
-              <br />
-              {'"a benevolent smile"'}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Learn More</Button>
-          </CardActions>
-        </Card>
-      </header>
-    </div>
+const App = () => (
+    <div>Typing Game</div>
   );
-}
 
 export default App;
