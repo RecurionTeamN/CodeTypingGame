237b6540775bc7a4725b403321561a5634bd7ff5
hiroto0222 <aoyamahiroto0222@gmail.com>
2022-03-06 15:02:17 +0900

    added pre-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./App.css";
 
-const App = () => (
-    <div>Typing Game</div>
-  );
+const App = () => {
+  return (
+    <>
+      <div>Typing Game</div>
+    </>
+  )
+};
 
 export default App;
