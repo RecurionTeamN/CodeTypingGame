03c8e15aeadf878a6619321c7bfd850c71bc9fec
Mori Yasuaki <mori.yasuaki18@gmail.com>
2022-03-08 04:23:16 +0900

    #18 add Result Page and its components again.

--- src/components/ResultPageHeader.tsx
+++ src/components/ResultPageHeader.tsx
@@ -4,16 +4,16 @@ import { ThemeProvider } from "@mui/styles";
 import theme from "../styles/Theme";
 
 const ResultPageHeader = () => (
-    <ThemeProvider theme={theme}>
-        <Box
-            sx={{
-                bgcolor:'primary.dark',
-                color: 'white'
-            }}
-        >
-            <h1>Result Page *これから開発する</h1>
-        </Box>
-    </ThemeProvider>
+  <ThemeProvider theme={theme}>
+    <Box
+      sx={{
+        bgcolor: "primary.dark",
+        color: "white",
+      }}
+    >
+      <h1>Result Page *これから開発する</h1>
+    </Box>
+  </ThemeProvider>
 );
 
 export default ResultPageHeader;
