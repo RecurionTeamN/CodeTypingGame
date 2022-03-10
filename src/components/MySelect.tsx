9adbe2da7c26640bb44fc096d2d9dbfd81613036
YIshihara11201 <yusukeishihara87@gmail.com>
2022-03-09 22:47:07 -0800

    #28_add_language_detection_functionality

--- src/components/MySelect.tsx
+++ src/components/MySelect.tsx
@@ -1,26 +1,24 @@
-
 import { SelectChangeEvent, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 import React from "react";
 
 type Props = {
-  label: string,
-  value: string,
-  options: string[],
-  onchange: (event:SelectChangeEvent<string>) => void
-}
+  label: string;
+  value: string;
+  options: string[];
+  onchange: (event: SelectChangeEvent<string>) => void;
+};
 
 const MySelect: React.VFC<Props> = ({ label, value, options, onchange }) => (
   <FormControl required>
     <InputLabel>{label}</InputLabel>
-    <Select
-      label={label.toLocaleLowerCase()}
-      value={value}
-      onChange={onchange}
-    >
-      {options.map((item, index) => <MenuItem key={item} value={index}>{item}</MenuItem>)}
+    <Select label={label.toLocaleLowerCase()} value={value} onChange={onchange}>
+      {options.map((item, index) => (
+        <MenuItem key={item} value={index}>
+          {item}
+        </MenuItem>
+      ))}
     </Select>
   </FormControl>
 );
-
 
 export default MySelect;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,98 +1,88 @@
-
-import {Modal, SelectChangeEvent, Button, TextField, Container, Stack, Box } from "@mui/material";
+import { Modal, SelectChangeEvent, Button, TextField, Container, Stack, Box } from "@mui/material";
 import React, { useState, useRef } from "react";
+import { detect } from "program-language-detector";
 import MySelect from "../components/MySelect";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: '50%',
-  height: '50%',
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "50%",
+  height: "50%",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
 
 const keyboards = ["Japan", "US"];
-const languages = ["Java", "JavaScript", "Python"];
 
 const HomePage = () => {
-  const nameRef = useRef('');
-  const codeRef = useRef('');
-  const maxLenRef = useRef('');
-  const personalCodeRef = useRef('');
+  const nameRef = useRef("");
+  const codeRef = useRef("");
+  const personalCodeRef = useRef("");
+  const languageRef = useRef("");
+  const personalLanguageRef = useRef("");
+  const maxLenRef = useRef("");
 
-  const [keyboard, setKeyboard] = useState('');
-  const [language, setLanguage] = useState('');
-  const [personalLanguage, setPesonalLanguage] = useState('');
-  const [personalSetting, setPersonalSetting] = useState({language:'', code:''});
+  const [keyboard, setKeyboard] = useState("");
+  const [personalSetting, setPersonalSetting] = useState({ language: "", code: "" });
 
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleNameChange = (event:React.ChangeEvent<HTMLInputElement>):void => {
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     nameRef.current = event.target.value;
   };
-  const handleCodeChange = (event:React.ChangeEvent<HTMLTextAreaElement>):void => {
+  const handleCodeChange = (event: React.ChangeEvent<HTMLTextAreaElement>): void => {
     codeRef.current = event.target.value;
+    languageRef.current = detect(event.target.value);
   };
-  const handleMaxLenChange = (event:React.ChangeEvent<HTMLInputElement>):void =>{
+  const handleMaxLenChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     maxLenRef.current = event.target.value;
-  }
-  const handlePersonalCodeChange = (event:React.ChangeEvent<HTMLTextAreaElement>):void => {
+  };
+  const handlePersonalCodeChange = (event: React.ChangeEvent<HTMLTextAreaElement>): void => {
     personalCodeRef.current = event.target.value;
+    personalLanguageRef.current = detect(event.target.value);
   };
 
-  const handleKeyboardChange = (event:SelectChangeEvent<string>):void => {
+  const handleKeyboardChange = (event: SelectChangeEvent<string>): void => {
     setKeyboard(event.target.value);
   };
-  const handleLanguageChange = (event:SelectChangeEvent<string>):void => {
-    setLanguage(event.target.value);
-  };
-  const handlePersonalLanguageChange = (event:SelectChangeEvent<string>):void => {
-    setPesonalLanguage(event.target.value);
-  };
-  
-  
-  const toggleModal = ():void => setIsOpen(!isOpen);
+
+  const toggleModal = (): void => setIsOpen(!isOpen);
 
   return (
-  <Container maxWidth="sm" sx={{ pt: 3 }}>
-    <div>
-      <p>Confirm states</p>
-      {`Name: ${nameRef.current}`}<br/>
-      {`KeyBoard: ${keyboard}`}<br/>
-      {`Language: ${language}`}<br/>
-      {`Code: ${codeRef.current}`}<br/>
-      {`MaxLength: ${maxLenRef.current}`}<br/>
-      {`Personal Setting: ${JSON.stringify(personalSetting)}`}<br/>
-    </div>
+    <Container maxWidth="sm" sx={{ pt: 3 }}>
+      <div>
+        <p>Confirm states</p>
+        {`Name: ${nameRef.current}`}
+        <br />
+        {`KeyBoard: ${keyboard}`}
+        <br />
+        {`Language: ${languageRef.current}`}
+        <br />
+        {`Code: ${codeRef.current}`}
+        <br />
+        {`MaxLength: ${maxLenRef.current}`}
+        <br />
+        {`Personal Setting: ${JSON.stringify(personalSetting)}`}
+        <br />
+      </div>
 
-    <h1>Code Typing Game App</h1>
+      <h1>Code Typing Game App</h1>
       <Stack spacing={3}>
-        <TextField
-          inputRef={nameRef}
-          required
-          label="Name"
-          onChange={handleNameChange}
-        />
+        <TextField inputRef={nameRef} required label="Name" onChange={handleNameChange} />
 
-        <MySelect
-          label="Keyboard Type"
-          value={keyboard}
-          options={keyboards}
-          onchange={handleKeyboardChange}
-        />
+        <MySelect label="Keyboard Type" value={keyboard} options={keyboards} onchange={handleKeyboardChange} />
 
-        <MySelect
+        {/* <MySelect
           label="Language"
           value={language}
           options={languages}
           onchange={handleLanguageChange}
-        />
-        
+        /> */}
+
         <TextField
           inputRef={codeRef}
           required
@@ -104,36 +94,33 @@ const HomePage = () => {
           variant="outlined"
           onChange={handleCodeChange}
         />
-        
-        <Box textAlign='center'>
-          <Button 
+
+        <Box textAlign="center">
+          <Button
             onClick={toggleModal}
             style={{
-              width: '30em',
+              width: "30em",
             }}
-          >Personal Setting</Button>
+          >
+            Personal Setting
+          </Button>
         </Box>
-        
+
         <Modal
           open={isOpen}
           onClose={toggleModal}
           aria-labelledby="personal-setting-modal"
           aria-describedby="set-personal-condition"
         >
-          <div>
-          <Box
-            sx={style} 
-            alignItems="center"
-            justifyContent="center"
-          >
+          <Box sx={style} alignItems="center" justifyContent="center">
             <p>Personal Setting</p>
             <Stack spacing={3} paddingTop={1}>
-              <MySelect
+              {/* <MySelect
                 label="Language"
                   value={personalLanguage}
                   options={languages}
                   onchange={handlePersonalLanguageChange}
-              />
+              /> */}
               <TextField
                 inputRef={personalCodeRef}
                 required
@@ -146,34 +133,32 @@ const HomePage = () => {
                 onChange={handlePersonalCodeChange}
               />
               <Button
-                onClick={()=>{
-                  setPersonalSetting({language:personalLanguage, code:personalCodeRef.current});
+                onClick={() => {
+                  setPersonalSetting({ language: personalLanguageRef.current, code: personalCodeRef.current });
                   toggleModal();
                 }}
               >
                 Confirm
               </Button>
-        </Stack>
-    </Box>
-          </div>
+            </Stack>
+          </Box>
         </Modal>
 
-        <TextField 
+        <TextField
           inputRef={maxLenRef}
           required
           type="number"
           label="Max number of letters"
-          InputProps={{inputProps: { min: 0}}}
+          InputProps={{ inputProps: { min: 0 } }}
           onChange={handleMaxLenChange}
         />
 
         <Button color="primary" variant="contained" size="large">
           Let&apos;s start Game!
         </Button>
-
       </Stack>
     </Container>
-  )
-}
-                      
+  );
+};
+
 export default HomePage;
