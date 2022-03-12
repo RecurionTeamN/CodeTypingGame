837d70b7c58e536f793070d822e3b1567e54cc3a
hiroto0222 <aoyamahiroto0222@gmail.com>
2022-03-12 20:12:11 +0900

    #38 added react-router-dom v6 and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import GamePage from "./pages/GamePage";
 import ResultPage from "./pages/ResultPage";
 
 const App = () => (
-  <div>
-    <HomePage />
-    <GamePage />
-    <ResultPage />
-  </div>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/game" element={<GamePage />} />
+      <Route path="/results" element={<ResultPage />} />
+      <Route path="*" element={<HomePage />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
