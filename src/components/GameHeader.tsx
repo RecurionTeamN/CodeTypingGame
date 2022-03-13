31043d31755d4fcdb5fcf46ae666a57a66c12cc5
hiroto0222 <aoyamahiroto0222@gmail.com>
2022-03-13 16:25:44 +0900

    #40 changed Container.tsx and tsconfig.json to support custom MUI themes

--- src/components/GameHeader.tsx
+++ src/components/GameHeader.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { AppBar, Typography, Box, Toolbar, Button } from "@mui/material";
+import { AppBar, Typography, Box, Toolbar, Button, Theme } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 import formatTime from "../utils/formatTime";
 
 type Props = {
@@ -8,29 +9,39 @@ type Props = {
   reset: VoidFunction;
 };
 
-const GameHeader: React.VFC<Props> = ({ timeTyping, missCount, reset }) => (
-  <Box>
-    <AppBar position="static" sx={{ p: 1 }}>
-      <Toolbar sx={{ justifyContent: "space-between" }}>
-        <Box>
-          <Typography variant="h3" component="div">
-            Typing Game
-          </Typography>
-        </Box>
-        <Box>
-          <Typography variant="h5" component="div">
-            タイム: <strong>{formatTime(timeTyping)}</strong>
-          </Typography>
-          <Typography variant="h5" component="div">
-            ミスタイプ: <strong>{missCount} 回</strong>
-          </Typography>
-          <Button variant="contained" color="inherit" onClick={reset}>
-            reset
-          </Button>
-        </Box>
-      </Toolbar>
-    </AppBar>
-  </Box>
-);
+const useStyles = makeStyles((theme: Theme) => ({
+  appbar: {
+    backgroundColor: theme.palette.primary.main,
+  },
+}));
+
+const GameHeader: React.VFC<Props> = ({ timeTyping, missCount, reset }) => {
+  const classes = useStyles();
+
+  return (
+    <Box>
+      <AppBar className={classes.appbar} position="static">
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <Box>
+            <Typography variant="h3" component="div">
+              Typing Game
+            </Typography>
+          </Box>
+          <Box>
+            <Typography variant="h5" component="div">
+              タイム: <strong>{formatTime(timeTyping)}</strong>
+            </Typography>
+            <Typography variant="h5" component="div">
+              ミスタイプ: <strong>{missCount} 回</strong>
+            </Typography>
+            <Button variant="contained" color="inherit" onClick={reset}>
+              reset
+            </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
 
 export default GameHeader;
--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -4,8 +4,7 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#77ffff",
-      dark: "#2bbeed",
+      main: "#2bbeed",
     },
     secondary: {
       main: "#8cb4c4",
