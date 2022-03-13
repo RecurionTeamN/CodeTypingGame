148277f9dc5f88c3e1ab406b851a8bdd4c8ac29f
Mori Yasuaki <mori.yasuaki18@gmail.com>
2022-03-14 06:37:37 +0900

    #26 add hand component

--- src/components/FingerStatistics.tsx
+++ src/components/FingerStatistics.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Box from "@mui/material/Box";
+import HandSVG from "./HandSVG";
 
 type Hand = "left" | "right";
 
@@ -20,25 +21,24 @@ type Props = {
 
 const FingerStatistics: React.VFC<Props> = ({ data }) => {
   // キー毎の情報を指ごとの情報に変換する
-  // Object.keys()の返り値がstring型になるので型アサーションを使用する
   const fingerData: {
     [key1 in Hand]: {
-      [key2 in Finger]: { pushCountSum: number; missCountSum: number; timeSecSum: number };
+      [key2 in Finger]: { pushCountSum: number; missCountSum: number; timeSecSum: number; color: string };
     };
   } = {
     left: {
-      thumb: { pushCountSum: 0, missCountSum: 0, timeSecSum: 0 },
-      first: { pushCountSum: 0, missCountSum: 0, timeSecSum: 0 },
-      second: { pushCountSum: 0, missCountSum: 0, timeSecSum: 0 },
-      third: { pushCountSum: 0, missCountSum: 0, timeSecSum: 0 },
-      fourth: { pushCountSum: 0, missCountSum: 0, timeSecSum: 0 },
+      thumb: { pushCountSum: 0, missCountSum: 0, timeSecSum: 0, color: "#fff" },
+      first: { pushCountSum: 0, missCountSum: 0, timeSecSum: 0, color: "#fff" },
+      second: { pushCountSum: 0, missCountSum: 0, timeSecSum: 0, color: "#fff" },
+      third: { pushCountSum: 0, missCountSum: 0, timeSecSum: 0, color: "#fff" },
+      fourth: { pushCountSum: 0, missCountSum: 0, timeSecSum: 0, color: "#fff" },
     },
     right: {
-      thumb: { pushCountSum: 0, missCountSum: 0, timeSecSum: 0 },
-      first: { pushCountSum: 0, missCountSum: 0, timeSecSum: 0 },
-      second: { pushCountSum: 0, missCountSum: 0, timeSecSum: 0 },
-      third: { pushCountSum: 0, missCountSum: 0, timeSecSum: 0 },
-      fourth: { pushCountSum: 0, missCountSum: 0, timeSecSum: 0 },
+      thumb: { pushCountSum: 0, missCountSum: 0, timeSecSum: 0, color: "#fff" },
+      first: { pushCountSum: 0, missCountSum: 0, timeSecSum: 0, color: "#fff" },
+      second: { pushCountSum: 0, missCountSum: 0, timeSecSum: 0, color: "#fff" },
+      third: { pushCountSum: 0, missCountSum: 0, timeSecSum: 0, color: "#fff" },
+      fourth: { pushCountSum: 0, missCountSum: 0, timeSecSum: 0, color: "#fff" },
     },
   };
   data.forEach((element) => {
@@ -48,8 +48,11 @@ const FingerStatistics: React.VFC<Props> = ({ data }) => {
   });
   // 指ごとの統計情報を表示する
   // 指ごとの統計情報に親指は不要なので各手の指配列をfilter()する
+  // Object.keys()の返り値がstring型になるので型アサーションを使用する
   const statistics = (Object.keys(fingerData) as Hand[]).map((hand) => {
-    const eachHandResults = (Object.keys(fingerData[hand]) as Finger[])
+    const fingerArr = Object.keys(fingerData[hand]) as Finger[];
+    if (hand === "left") fingerArr.reverse();
+    const eachHandResults = fingerArr
       .filter((finger) => finger !== "thumb")
       .map((finger) => {
         const currFinData = fingerData[hand][finger];
@@ -57,18 +60,33 @@ const FingerStatistics: React.VFC<Props> = ({ data }) => {
           ((currFinData.pushCountSum - currFinData.missCountSum) / currFinData.pushCountSum) * 100
         );
         const speed = (currFinData.pushCountSum - currFinData.missCountSum) / (currFinData.timeSecSum / 60);
+
+        if (speed >= 80) fingerData[hand][finger].color = "#0000FF";
+        else if (speed >= 60) fingerData[hand][finger].color = "#0099FF";
+        else if (speed >= 40) fingerData[hand][finger].color = "#00CCFF";
+        else if (speed >= 20) fingerData[hand][finger].color = "#00FFFF";
+        else fingerData[hand][finger].color = "#CCFFFF";
+
         return (
-          <div key={finger}>
-            <p>
-              {finger}: {accuracy}%, {speed}wpm
-            </p>
-          </div>
+          <Box key={finger} sx={{ px: 2 }}>
+            {finger} <br />
+            {accuracy}% <br />
+            {speed}wpm <br />
+          </Box>
         );
       });
+
     return (
       <div key={hand}>
-        <h4>{hand}</h4>
-        {eachHandResults}
+        <h4>{hand} fingers</h4>
+        <HandSVG
+          hand={hand}
+          firstColor={fingerData[hand].first.color}
+          secondColor={fingerData[hand].second.color}
+          thirdColor={fingerData[hand].third.color}
+          fourthColor={fingerData[hand].fourth.color}
+        />
+        <Box sx={{ display: "flex", justifyContent: "space-evenly" }}>{eachHandResults}</Box>
       </div>
     );
   });
@@ -76,9 +94,54 @@ const FingerStatistics: React.VFC<Props> = ({ data }) => {
   // 後々に手のコンポーネントを作成して、指毎の統計を見やすいデザインとする。
   return (
     <div>
-      <h3>Finger Master</h3>
-      <p>*指毎の統計情報表示をこれから詳細に開発する。以下は現在の例</p>
-      <Box sx={{ display: "flex", justifyContent: "space-evenly" }}>{statistics}</Box>
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+        <h3>Finger Master</h3>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <p>slow</p>
+          <Box
+            sx={{
+              width: 20,
+              height: 20,
+              bgcolor: "#CCFFFF",
+              m: 1,
+            }}
+          />
+          <Box
+            sx={{
+              width: 20,
+              height: 20,
+              bgcolor: "#00FFFF",
+              m: 1,
+            }}
+          />
+          <Box
+            sx={{
+              width: 20,
+              height: 20,
+              bgcolor: "#00CCFF",
+              m: 1,
+            }}
+          />
+          <Box
+            sx={{
+              width: 20,
+              height: 20,
+              bgcolor: "#0099FF",
+              m: 1,
+            }}
+          />
+          <Box
+            sx={{
+              width: 20,
+              height: 20,
+              bgcolor: "#0000FF",
+              m: 1,
+            }}
+          />
+          <p>fast</p>
+        </Box>
+      </Box>
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>{statistics}</Box>
     </div>
   );
 };
