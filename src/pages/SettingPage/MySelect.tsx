3656d8669a4acc706c62ca5029e26798134f7c3c
YIshihara11201 <yusukeishihara87@gmail.com>
2022-04-04 01:20:59 -0700

    #109 fixed Select component's defaultValue props inside Myselect component

--- src/pages/SettingPage/MySelect.tsx
+++ src/pages/SettingPage/MySelect.tsx
@@ -1,5 +1,5 @@
 import { SelectChangeEvent, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
-import React from "react";
+import React, { useRef } from "react";
 
 type Props = {
   label: string;
@@ -8,22 +8,26 @@ type Props = {
   onchange: (event: SelectChangeEvent<string>) => void;
 };
 
-const MySelect: React.VFC<Props> = ({ label, options, defaultValue = "", onchange }) => (
-  <FormControl required>
-    <InputLabel>{label}</InputLabel>
-    <Select
-      id={label.toLocaleLowerCase().replace(" ", "-")}
-      label={label.toLocaleLowerCase().replace(" ", "-")}
-      defaultValue={defaultValue}
-      onChange={onchange}
-    >
-      {options.map((item) => (
-        <MenuItem key={item} value={item}>
-          {item}
-        </MenuItem>
-      ))}
-    </Select>
-  </FormControl>
-);
+const MySelect: React.VFC<Props> = ({ label, options, defaultValue = "", onchange }) => {
+  const defaultRef = useRef(defaultValue);
+  return (
+    <FormControl required>
+      <InputLabel>{label}</InputLabel>
+      <Select
+        key={label}
+        id={label.toLocaleLowerCase().replace(" ", "-")}
+        label={label.toLocaleLowerCase().replace(" ", "-")}
+        defaultValue={defaultRef.current ?? ""}
+        onChange={onchange}
+      >
+        {options.map((item) => (
+          <MenuItem key={item} value={item}>
+            {item}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
 
 export default MySelect;
